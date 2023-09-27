9f0d0fc97489ecab586296938f720ea44c01b0fa
Felipe Las Heras <felipe.las.heras@gmail.com>
2023-09-27 16:12:52 -0300

    primer avance

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-//Crear carta con DOM
-const card = document.querySelector('.card');
-const cardNumber = document.querySelector('.card-number');
-//Div de la carta
-const topDiv = document.querySelector('.top');
-const middleDiv = document.querySelector('.middle');
-const bottomDiv = document.querySelector('.bottom');
-//Crear número de carta
-const number = document.createElement('p');
-number.classList.add('number');
-//Crear símbolo de carta
-const symbol = document.createElement('p');
-symbol.classList.add('symbol');
+document.addEventListener('DOMContentLoaded', function() {
+  const card = document.querySelector('.card');
+  const cardValue = document.querySelector('.card-value');
+  const cardSuit = document.querySelector('.card-suit');
+  const cardSuitTop = document.querySelector('.card-suit-top');
+  const cardSuitBottom = document.querySelector('.card-suit-bottom');
+  const button = document.querySelector('.button');
+  const suits = ["♠", "♣", "♥", "♦"];
+  const values = [];
+for (let i = 2; i <= 10; i++) {
+    values.push(i);
+  }
+  values.push("J", "Q", "K", "A");
+  const deck = [];
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      deck.push(values[j] + suits[i]);
+    }
+  }
+  button.addEventListener('click', function() {
+    const randomCard = deck[Math.floor(Math.random() * deck.length)];
+    cardValue.innerHTML = randomCard[0];
+    cardSuit.innerHTML = randomCard[1];
+    cardSuitTop.innerHTML = randomCard[1];
+    cardSuitBottom.innerHTML = randomCard[1];
+    if (randomCard[1] === "♥" || randomCard[1] === "♦") {
+      card.classList.add('red');
+    } else {
+      card.classList.remove('red');
+    }
+  });});
